fddee1d86b987ccdc769d0311230aac33e46f6e8
Jared Nielson <jnielson@kyanicorp.com>
2021-11-10 21:07:40 -0700

    Add collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
