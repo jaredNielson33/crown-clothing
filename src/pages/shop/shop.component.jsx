d6c8d9c5bc9e8141c5831dfe60fc180b5c86d72f
Jared Nielson <jnielson@kyanicorp.com>
2021-11-12 23:09:05 -0700

    moved directory and shop data into its own reducer. Created selectors for respective reducers and connect it to components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { createStructuredSelector } from "reselect";
-import { connect } from "react-redux";
+import { Route } from "react-router";
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectShopItems } from "../../redux/shop/shop.selectors";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-    ))}
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopItems,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
